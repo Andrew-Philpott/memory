57ebbd2d6fc0c664e6866cfa767d15edfab3400d
Andrew Philpott <andrewphilpott92@gmail.com>
2020-02-04 22:09:05 -0800

    Fix cardClickListener logic to not increase the click count when the same card is clicked twice. Fix formatting.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 #### _Memory Game_, 01.31.2020_
 #### By _**Andrew Philpott and Steven Fleming**_
 ## Description
-_The purpose of this application is to provide a user a fun Memory card game._
+_The purpose of this application is to provide a fun Memory card game. The objective of the game is to identify all pairs of cards by selecting matching cards back to back. Change game difficulty by entering the number of pairs of cards that you would like to play with._
 
 ## Specs
 * _Spec: _
--- src/js/interfacelogic.js
+++ src/js/interfacelogic.js
@@ -13,23 +13,23 @@ export function randomizePairs(selectionArray) {
   let initialSelectionArrayLength = selectionArray.length;
 
   for(let i = 0; i < initialSelectionArrayLength; i++) {
-      let currentSelectionArrayLength = selectionArray.length;
-      let value = Math.floor((Math.random() * (currentSelectionArrayLength-1)));
-      newArray.push(selectionArray[value]);
-      selectionArray.splice(value,1);
+    let currentSelectionArrayLength = selectionArray.length;
+    let value = Math.floor((Math.random() * (currentSelectionArrayLength-1)));
+    newArray.push(selectionArray[value]);
+    selectionArray.splice(value,1);
   }
   return newArray;
 }
 
 export function createGameListener() {
-    let inputtedDifficulty = parseInt($("#difficulty-input").val());
-    createGame(inputtedDifficulty);
+  let inputtedDifficulty = parseInt($("#difficulty-input").val());
+  createGame(inputtedDifficulty);
 }
 
 export function createGame(inputtedDifficulty) {
-    let cards = defineCards(inputtedDifficulty);
-    let arr = randomizePairs(cards);
-    displayPairs(arr);
+  let cards = defineCards(inputtedDifficulty);
+  let arr = randomizePairs(cards);
+  displayPairs(arr);
 }
 
 export function displayPairs(cards) {
@@ -41,37 +41,32 @@ export function displayPairs(cards) {
   return gameDisplay.html(cardsHtml);
 }
 
-export function pageListener() {
+export function cardClickListener() {
   let numberOfClicks = 0;
-  let cardOne = "";
-  let cardTwo = "";
+  let cardOneText = "";
+  let cardTwoText = "";
   let cardOneElement = "";
   let cardTwoElement = "";
   $("#game-display").on("click", ".card", function() {
-    numberOfClicks++;
     let hiddenCardElement = $(this).find(".card-text");
-    // .card-text
-    
-    if((numberOfClicks % 2) === 1) {
+    if(!(hiddenCardElement.hasClass("clicked"))) {
+      numberOfClicks++;
+      hiddenCardElement.addClass("clicked");
       hiddenCardElement.removeClass("hidden");
-      cardOneElement = hiddenCardElement;
-      cardOne = hiddenCardElement.text();
-      console.log(cardOne);
-    } else {
-      hiddenCardElement.removeClass("hidden");
-      cardTwoElement = hiddenCardElement;
-      cardTwo = hiddenCardElement.text();
-      if(cardOne === cardTwo) {
-        console.log("match");
+      if((numberOfClicks % 2) === 1) {
+        cardOneElement = hiddenCardElement;
+        cardOneText = cardOneElement.text();
       } else {
+        cardTwoElement = hiddenCardElement;
+        cardTwoText = hiddenCardElement.text();
+        if(cardOneText === cardTwoText) {
+        } else {
+        cardOneElement.removeClass("clicked");
+        cardTwoElement.removeClass("clicked");
         cardOneElement.addClass("hidden");
         cardTwoElement.addClass("hidden");
-        cardOne = "";
-        cardTwo = "";
-        cardOneElement = "";
-        cardTwoElement = "";
+        }
       }
     }
   });
-  };
-  
+}
